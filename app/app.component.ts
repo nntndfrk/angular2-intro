fce7726ab57df49463795edf92dfb61db1a8a51b
nntndfrk <nntndfrk@gmail.com>
2017-04-22 10:44:54 +0300

    Привязка данных

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 const todos = [
-	'Изучить JavaScript',
-	'Изучить Angular 2',
-	'Изучить TypeScript',
+	{
+		title: 'Изучить JavaScript',
+		completed: true
+	},
+	{
+		title: 'Изучить Angular 2',
+		completed: false
+	},
+	{
+		title: 'Изучить TypeScript',
+		comleted: false
+	}
 ];
 
 @Component({
@@ -15,4 +24,16 @@ const todos = [
 export class AppComponent {
 	title = 'Angular 2Do';
 	todos = todos;
+
+	toggle(todo: any) {
+		todo.completed = !todo.completed;
+	}
+
+	delete(todo: any) {
+		let index = this.todos.indexOf(todo);
+
+		if (index > -1) {
+			this.todos.splice(index, 1);
+		}
+	}
 }

894ea8e818d022ebe8b9ea0e59f0f7804bccad2a
nntndfrk <nntndfrk@gmail.com>
2017-04-23 11:39:35 +0300

    Декомпозиция

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,54 +1,9 @@
 import { Component } from '@angular/core';
 
-class Todo {
-
-	constructor(
-		public title: string, 
-		public completed: boolean = false) {}
-}
-
-const todos: Todo[] = [
-	{
-		title: 'Изучить JavaScript',
-		completed: true
-	},
-	{
-		title: 'Изучить Angular 2',
-		completed: false
-	},
-	{
-		title: 'Изучить TypeScript',
-		completed: false
-	}
-];
-
 @Component({
 	moduleId: module.id,
 	selector: 'app',
 	templateUrl: 'app.component.html',
 	styleUrls: ['app.component.css']
 })
-export class AppComponent {
-	title: string = 'Angular 2Do';
-	todos: Todo[] = todos;
-	newTodoTitle: string = '';
-
-	create() {
-		let todo: Todo = new Todo(this.newTodoTitle);
-
-		this.todos.push(todo);
-		this.newTodoTitle = '';
-	}
-
-	toggle(todo: Todo) {
-		todo.completed = !todo.completed;
-	}
-
-	delete(todo: Todo) {
-		let index = this.todos.indexOf(todo);
-
-		if (index > -1) {
-			this.todos.splice(index, 1);
-		}
-	}
-}
+export class AppComponent {}
